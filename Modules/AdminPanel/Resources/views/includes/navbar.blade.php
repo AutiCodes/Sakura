a91e58a5bfa1ff0ddde5d220a3c4f75d45e99730
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-10-31 18:59:40 +0100

    Added navbar link, db prefix

--- Modules/AdminPanel/Resources/views/includes/navbar.blade.php
+++ Modules/AdminPanel/Resources/views/includes/navbar.blade.php
@@ -34,35 +34,35 @@
                         <span class="font-weight-bold">Dit is het beste artikel! OMG!</span>
                     </div>
                 </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Laat alle comments zien</a>
+                <a class="dropdown-item text-center small text-gray-500" href="/admin/comments">Laat alle comments zien</a>
             </div>
         </li>
 
         <!-- Nav Item - New actions -->
         <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="#" id="newDropdown" role="button"
+          <a class="nav-link dropdown-toggle" id="newDropdown" role="button"
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-plus fa-fw"></i>
           </a>
           <!-- Dropdown - New actions -->
           <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="newDropdown">
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="/admin/berichten/toevoegen">
                 <i class="fas fa-newspaper fa-sm fa-fw mr-2 text-gray-400"></i>
                 Artikel
             </a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="/admin/media/toevoegen">
                 <i class="fas fa-image fa-sm fa-fw mr-2 text-gray-400"></i>
                 Media
             </a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="/admin/paginas/toevoegen">
                 <i class="fa-regular fa-file-lines fa-sm fa-fw mr-2 text-gray-400"></i>
                 Pagina
             </a>                          
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="/admin/gebruikers/toevoegen">
                 <i class="fa fa-person fa-sm fa-fw mr-2 text-gray-400"></i>
                 Gebruiker
             </a>                                
@@ -99,12 +99,12 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="/admin/gebruikers/profiel/231321">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profiel bewerken
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="/admin/uitloggen">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Uitloggen
                 </a>
