65d8e8cb4ebe5d2d7ff88c550b9a692f7f55b966
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2024-01-28 13:17:50 +0100

    Updated image finder

--- app/Helpers/FirstImageArticleFinder.php
+++ app/Helpers/FirstImageArticleFinder.php
@@ -3,7 +3,7 @@
 if (!function_exists('FirstImgFinder')) {
 
     /**
-     * Finds the first image in the given HTML data and returns it's path OR NULL if there isn't an image
+     * Finds the first image in the given HTML data and returns it's path OR default image if no image was found
      *
      * @param string $data
      * @return string 
@@ -11,6 +11,11 @@ if (!function_exists('FirstImgFinder')) {
     function FirstImgFinder($data)
     {
         preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $data, $image);
+
+        if (!isset($image['src'])) {
+            return url('/system/ArticleDefault.jpg');
+        }
+
         return $image['src'];
     }
 }
